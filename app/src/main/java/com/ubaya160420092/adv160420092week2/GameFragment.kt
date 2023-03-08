384a4144c9f9dbcd197c6138c72baf683bd5f156
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2023-03-08 14:11:44 +0700

    init

---
+++ app/src/main/java/com/ubaya160420092/adv160420092week2/GameFragment.kt
@@ -0,0 +1,33 @@
+package com.ubaya160420092.adv160420092week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnBack=view.findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener{
+            val action=GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
